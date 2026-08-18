1662f321ef9a1430054f5721359d1edde810e99e
Zalahk417 <pmhaik@outlook.com>
2026-08-18 15:37:26 +0800

    Cache customer-facing WAPAD v4 assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wapad-v3-20260818';
-const CORE=['/','/index.html','/assessment.html','/knowledge.html','/privacy.html','/sources.html','/assets/v2.css','/assets/v2.js','/assets/v3.css','/assets/v3.js','/assets/assessment.js','/assets/favicon.svg'];
+const CACHE='wapad-v4-customer-20260818';
+const CORE=['/','/index.html','/assessment.html','/knowledge.html','/privacy.html','/sources.html','/assets/v2.css','/assets/v2.js','/assets/v3.css','/assets/v3.js','/assets/customer.css','/assets/customer.js','/assets/assessment.js','/assets/favicon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{
